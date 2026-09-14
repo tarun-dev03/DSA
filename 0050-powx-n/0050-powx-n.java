d193faf16a582b6409387568d80320da4520df34
Tarun <143314317+tarun-dev03@users.noreply.github.com>
2026-09-15 00:00:11 +0530

    Time: 0 ms (100%), Space: 48 MB (46.04%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,30 @@
+class Solution {
+
+    public double myPow(double x, int n) {
+
+        long N = n;
+
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+
+        return power(x, N);
+    }
+
+    public double power(double x, long n) {
+
+        // Base case
+        if (n == 0) {
+            return 1;
+        }
+
+        double half = power(x, n / 2);
+
+        if (n % 2 == 0) {
+            return half * half;
+        }
+
+        return half * half * x;
+    }
+}
