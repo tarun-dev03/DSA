eb39c875300bf9be02ccd8e3198250648dac8931
Tarun <143314317+tarun-dev03@users.noreply.github.com>
2026-09-23 01:33:51 +0530

    Time: 230 ms (60.5%), Space: 42.9 MB (61.47%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -0,0 +1,76 @@
+class Solution {
+
+    public void solveSudoku(char[][] board) {
+        solve(board);
+    }
+
+    boolean solve(char[][] board) {
+
+        // Find an empty cell
+        for (int row = 0; row < 9; row++) {
+
+            for (int col = 0; col < 9; col++) {
+
+                if (board[row][col] == '.') {
+
+                    // Try numbers 1 to 9
+                    for (char num = '1'; num <= '9'; num++) {
+
+                        if (isValid(board, row, col, num)) {
+
+                            // Place the number
+                            board[row][col] = num;
+
+                            // Solve the remaining board
+                            if (solve(board)) {
+                                return true;
+                            }
+
+                            // Backtrack
+                            board[row][col] = '.';
+                        }
+                    }
+
+                    // No number worked
+                    return false;
+                }
+            }
+        }
+
+        // No empty cells left
+        return true;
+    }
+
+    boolean isValid(char[][] board, int row, int col, char num) {
+
+        // Check row
+        for (int j = 0; j < 9; j++) {
+            if (board[row][j] == num) {
+                return false;
+            }
+        }
+
+        // Check column
+        for (int i = 0; i < 9; i++) {
+            if (board[i][col] == num) {
+                return false;
+            }
+        }
+
+        // Check 3x3 box
+        int startRow = (row / 3) * 3;
+        int startCol = (col / 3) * 3;
+
+        for (int i = startRow; i < startRow + 3; i++) {
+
+            for (int j = startCol; j < startCol + 3; j++) {
+
+                if (board[i][j] == num) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
